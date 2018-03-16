9771236257e7ed8ee7ae9b6b0098c4d2e196a273
misiaxis <michal.pr@o2.pl>
2018-03-16 14:44:21 +0100

    Rozpoczęcie implementacji klasy DNSwatcher
    Dodanie do solucji projektu DNSwatcher, mającego stanowić element końcowego systemu.

---
+++ DNSwatcher/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNSwatcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DNSwatcher dnsWatcher=new DNSwatcher(true);
+            dnsWatcher.debug();
+            Console.ReadLine();
+        }
+    }
+}
