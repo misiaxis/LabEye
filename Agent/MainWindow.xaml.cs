31b56b800ae11fedfb8eb8e3da913728995e4437
misiaxis <michal.pr@o2.pl>
2018-03-20 18:02:50 +0100

    Rozpoczęcie pracy nad agentem
    Proszę niczego nie ruszać, prace w budowie!

---
+++ Agent/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Agent
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //Now it will startup maximized and user will be forced to log in
+            this.WindowState = WindowState.Maximized;
+
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            //Canceling closing aplication
+            e.Cancel = true;
+        }
+    }
+}
