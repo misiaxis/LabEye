27dc86bec5f1fe6e6a0f5442aedae90d305620bc
misiaxis <michal.pr@o2.pl>
2018-03-22 10:59:00 +0100

    Aplikacja administratora, z zakładką dla czarnych list
    Początek pracy

---
+++ Administrator/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Administrator
+{
+    public partial class Form1 : Form
+    {
+        private void UpdateAppsListBox()
+        {
+            //get list from database
+            /*
+            foreach (string blackkeyword in blacklist)
+            {
+                BlackListAppListBox.Items.Add(blackkeyword);
+            }
+            */
+        }
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            BlackListAppListBox.Text = "";
+            BlackListAppListBox.Items.Add("Wpis");
+        }
+
+        private void AddNewKeywordToBlacklist(object sender, EventArgs e)
+        {
+            //sendtodatabasenewappblackkeyword(appkeywordtoadd.Text)
+            BlackListAppListBox.Items.Add(appkeywordtoadd.Text);
+        }
+    }
+}
